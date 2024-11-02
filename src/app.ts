05b798fc687128f92bb2505e91d383de0b6b235e
Teemu Tanninen <teemuxtanninen@gmail.com>
2024-11-02 05:35:01 +0200

    feat: update build scripts and TypeScript config
    - Added new build and dev scripts in package.json

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+console.log("Hello World!");
+
+type TVehicle = {
+  model: string;
+  color: string;
+  year: number;
+  power: number;
+};
+
+const Vehicle: TVehicle = {
+  model: "Boring generic vehicle",
+  color: "Red",
+  year: 1993,
+  power: 60,
+};
+
+console.log(Vehicle);
