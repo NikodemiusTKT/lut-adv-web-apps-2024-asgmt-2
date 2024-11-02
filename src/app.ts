0cdb6a78d7f28c49450d05d434a8c3411fbd2bdb
Teemu Tanninen <teemuxtanninen@gmail.com>
2024-11-02 05:45:04 +0200

    feat: add interfaces and sample objects for vehicles
    - Added IVehicle, ICar, IBoat, and IPlane interfaces
    - Created sample objects for Car, Plane, and Boat
    - Logged the array of vehicles to the console

--- src/app.ts
+++ src/app.ts
@@ -15,3 +15,49 @@ const Vehicle: TVehicle = {
 };
 
 console.log(Vehicle);
+
+interface IVehicle {
+  model: string;
+  color: string;
+  year: number;
+  power: number;
+}
+
+interface ICar extends IVehicle {
+  bodyType: string;
+  wheelCount: number;
+}
+interface IBoat extends IVehicle {
+  draft: number;
+}
+interface IPlane extends IVehicle {
+  wingSpan: number;
+}
+
+const Car: ICar = {
+  model: "Ford focus",
+  color: "Green",
+  year: 2016,
+  power: 150,
+  bodyType: "Hatchback",
+  wheelCount: 4,
+};
+
+const Plane: IPlane = {
+  model: "Boeing 777",
+  color: "White",
+  year: 2020,
+  power: 170000,
+  wingSpan: 65,
+};
+
+const Boat: IBoat = {
+  model: "Bella",
+  color: "Black",
+  year: 2022,
+  power: 100,
+  draft: 0.42,
+};
+
+const vehicles: Array<IVehicle> = [Car, Plane, Boat];
+console.log(vehicles);
